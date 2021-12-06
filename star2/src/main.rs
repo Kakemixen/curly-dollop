57834878fd81b0306060d3def9770d6a332dade8
Erling Ljunggren <hljunggr@cisco.com>
2021-12-06 15:54:12 +0100

    star2 attempt 1

---
+++ star2/src/main.rs
@@ -0,0 +1,25 @@
+use std::fs;
+use num;
+
+fn main() {
+    let filename = "input.txt";
+    let contents = fs::read_to_string(filename).expect("can't read file");
+    let arr = contents.split("\n")
+        .filter(|x| {!x.is_empty()});
+    let arr: Vec<i32> = arr.map(|x| { 
+        x.parse::<i32>().expect("can't convert string to int")
+    }).collect();
+    let mut acc = 0;
+    for i in 1..arr.len()-3 {
+        if trisum(&arr, i) > trisum(&arr, i-1) {
+            acc += 1;
+        }
+    }
+    println!("{}", acc);
+}
+
+fn trisum<'a, T>(arr: &'a [T], start: usize) -> T
+    where T: num::Integer + std::iter::Sum<&'a T>
+{
+    arr[start..start+3].iter().sum()
+}
