2a2c33ea7b74ef4c96ca8751210e79b698d3f350
Erling Ljunggren <hljunggr@cisco.com>
2021-12-07 10:47:58 +0100

    easier return type

--- aoclib/src/lib.rs
+++ aoclib/src/lib.rs
@@ -4,12 +4,15 @@ pub mod fileops {
     use std::io::{self, BufRead};
 
     pub fn get_file_lines<P>(path: P)
-        -> io::Lines<io::BufReader<File>>
+        -> impl Iterator<Item = String>
     where P: AsRef<Path>
     {
         let file = File::open(path).expect("could open file!");
         println!("test");
         io::BufReader::new(file).lines()
+            .map(|x| {
+                x.expect("couldn't read line!")
+            })
     }
 }
 
--- star3/src/main.rs
+++ star3/src/main.rs
@@ -3,8 +3,6 @@ use aoclib::fileops;
 fn main() {
     let lines = fileops::get_file_lines("input2.txt");
     for line in lines {
-        if let Ok(l) = line {
-            println!("{}", l);
-        }
+        println!("{}", line);
     }
 }
