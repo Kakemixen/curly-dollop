e20c69180aef2fe116b6d34edf60f3f6c9665f76
Erling Ljunggren <erlingljunggren@gmail.com>
2021-12-09 23:49:27 +0100

    star6

--- star6/src/main.rs
+++ star6/src/main.rs
@@ -7,11 +7,14 @@ fn main() {
         .map(|x| {
             bitops::bitstr_to_num(&x)
         }).collect();
-    let _oxygen = do_filter(&lines, NUM_BITS_IN_INPUT, true);
-    println!("{:?}", _oxygen);
-    let _scrubber = do_filter(&lines, NUM_BITS_IN_INPUT, false);
+    let oxygen = do_filter(&lines, NUM_BITS_IN_INPUT, true);
+    println!("{:?}", oxygen);
+    println!("{:b}", oxygen[0]);
+    let scrubber = do_filter(&lines, NUM_BITS_IN_INPUT, false);
     println!();
-    println!("{:?}", _scrubber);
+    println!("{:?}", scrubber);
+    println!("{:b}", scrubber[0]);
+    println!("{}", oxygen[0] * scrubber[0]);
 }
 
 fn do_filter(vec: &[i32], bit: i32, most_common: bool)
@@ -28,16 +31,22 @@ fn do_filter(vec: &[i32], bit: i32, most_common: bool)
                 acc
             }
         });
-    let mcb = if bitsum >= vec.len() as i32 / 2 {
+    let mcb = if bitsum as f32 >= vec.len() as f32 / 2.0 {
         1
     } else {
         0
     };
-    let choice = if most_common {
+    let mut choice = if most_common {
         mcb
     } else {
         (mcb + 1) % 2
     };
+    if bitsum == 0 {
+        choice = 0;
+    }
+    if bitsum == vec.len() as i32 {
+        choice = 1;
+    }
     println!("{:12b}", choice << bit);
     let filtered: Vec<i32> = vec.iter()
             .filter(|&&x| { ((x >> bit) & 1) == choice })
