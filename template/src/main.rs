5281e5ba0cda46f5f177d2bf2201a96b478b3289
Erling Ljunggren <erlingljunggren@gmail.com>
2021-12-18 19:33:39 +0100

    template for new days

---
+++ template/src/main.rs
@@ -0,0 +1,27 @@
+use aoclib::fileops;
+
+fn main() {
+    part1();
+    part2();
+}
+
+fn part1()
+    -> ()
+{
+    println!("part1 {}", "TODO");
+}
+
+fn part2()
+    -> ()
+{
+    println!("part2 {}", "TODO");
+}
+
+#[cfg(test)]
+mod tests
+{
+    #[test]
+    fn test_0x0001()
+    {
+    }
+}
