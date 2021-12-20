08d8162381c4d05bbb0b8224d5a2495ebd3fc01c
Erling Ljunggren <erlingljunggren@gmail.com>
2021-12-20 20:23:12 +0100

    day9: star2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,7 @@
 use aoclib::fileops;
 use itertools::Itertools;
 use ndarray::prelude::*;
+use std::collections::VecDeque;
 
 fn main() {
     part1();
@@ -20,7 +21,12 @@ fn part1()
 fn part2()
     -> ()
 {
-    println!("part2 {}", "TODO");
+    let input = fileops::get_file_lines("input.txt");
+    let map = parse_lines(input);
+    let lowest_points = find_lowest_points(&map);
+    let basins = find_basins(&map, &lowest_points);
+    let basin_score = calc_biggest_basin_prod(&basins);
+    println!("part2 {}", basin_score);
 }
 
 fn parse_lines(lines: impl Iterator<Item = String>)
@@ -42,7 +48,6 @@ fn find_lowest_points(map: &Array2<usize>)
     let mut lowest_map = Vec::new();
     for (index, _) in map.indexed_iter() {
         let lowest = is_lowest(map, index);
-        //println!("{:?} - lowest: {}", index, lowest);
         if lowest {
             lowest_map.push(index);
         }
@@ -63,7 +68,6 @@ fn is_lowest(map: &Array2<usize>, pos: (usize, usize))
             || h as usize >= map.dim().0 {
             continue;
         }
-        //println!("{:?} vs {:?}", map[pos], map[(h as usize,pos.1)]);
         if map[pos] >= map[(h as usize,pos.1)] {
             return false;
         }
@@ -74,12 +78,77 @@ fn is_lowest(map: &Array2<usize>, pos: (usize, usize))
             || w as usize >= map.dim().1 {
             continue;
         }
-        //println!("{:?} vs {:?}", map[pos], map[(pos.0, w as usize)]);
         if map[pos] >= map[(pos.0,w as usize)] {
             return false;
         }
     }
     true
+}
+
+fn find_adjecents(map: &Array2<usize>, pos: (usize, usize))
+    -> Vec<(usize,usize)>
+{
+    let h = pos.0 as i32;
+    let w = pos.1 as i32;
+    let mut adjecents = Vec::new();
+
+    for h in h-1..=h+1 {
+        if h as usize == pos.0
+            || h < 0
+            || h as usize >= map.dim().0 {
+            continue;
+        }
+        adjecents.push((h as usize, pos.1));
+    }
+    for w in w-1..=w+1 {
+        if w as usize == pos.1
+            || w < 0
+            || w as usize >= map.dim().1 {
+            continue;
+        }
+        adjecents.push((pos.0,w as usize));
+    }
+    adjecents
+}
+
+fn find_basins(map: &Array2<usize>, lowest_points: &Vec<(usize, usize)>)
+    -> Vec<Vec<(usize, usize)>>
+{
+    let mut basins = Vec::new();
+    for lowest in lowest_points {
+        basins.push(expand_basin(map, VecDeque::from(vec![*lowest]), Vec::new()));
+    }
+    basins
+}
+
+fn expand_basin(map: &Array2<usize>
+        , mut horizon: VecDeque<(usize, usize)>
+        , mut total: Vec<(usize, usize)>
+)
+    -> Vec<(usize,usize)>
+{
+    while let Some(point) = horizon.pop_front() {
+        total.push(point);
+        for adjecent in find_adjecents(map, point) {
+            if map[adjecent] != 9
+                && !total.contains(&adjecent)
+                && !horizon.contains(&adjecent)
+            {
+                horizon.push_back(adjecent);
+            }
+        }
+    }
+    total
+}
+
+fn calc_biggest_basin_prod(basins: &Vec<Vec<(usize,usize)>>)
+    -> usize
+{
+    let mut basin_scores: Vec<usize> = basins.iter().map(|x| {
+            x.len()
+        }).sorted().collect();
+    assert!(basin_scores.len() >= 3);
+    basin_scores.pop().unwrap() * basin_scores.pop().unwrap() * basin_scores.pop().unwrap()
 }
 
 fn calc_risk(map: &Array2<usize>, lowest_points: &Vec<(usize,usize)>)
@@ -139,5 +208,16 @@ mod tests
         let lowest_points = find_lowest_points(&input);
         let risk_sum = calc_risk(&input, &lowest_points);
         assert_eq!(risk_sum, 15);
+    }
+
+    #[test]
+    /// test finding the lowest point
+    fn test_0x0004()
+    {
+        let input = get_test_input();
+        let lowest_points = find_lowest_points(&input);
+        let basins = find_basins(&input, &lowest_points);
+        assert_eq!(basins.len(), 4);
+        assert_eq!(calc_biggest_basin_prod(&basins), 1134);
     }
 }
