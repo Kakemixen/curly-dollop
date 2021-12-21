322e8bda457a6721faea61167f0c94d2bf39f68e
Erling Ljunggren <erlingljunggren@gmail.com>
2021-12-21 12:29:47 +0100

    day10: star2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,4 +1,5 @@
 use aoclib::fileops;
+use itertools::Itertools;
 
 fn main() {
     part1();
@@ -10,8 +11,11 @@ fn part1()
 {
     let lines = fileops::get_file_lines("input.txt");
     let score: usize = lines.map( | x | {
-        let corrupted = find_error(&x);
-        score_error(&corrupted)
+        if let LineFix::Corrupted(corrupted) = find_error(&x){
+            score_corruption(&corrupted)
+        } else {
+            0
+        }
     }).sum();
     println!("part1 {}", score);
 }
@@ -19,7 +23,12 @@ fn part1()
 fn part2()
     -> ()
 {
-    println!("part2 {}", "TODO");
+    let lines = fileops::get_file_lines("input.txt");
+    let fixes: Vec<LineFix> = lines.map( |x| {
+        find_error(&x)
+    }) .collect();
+    let score = score_completions(&fixes);
+    println!("part2 {}", score);
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -59,8 +68,14 @@ impl Chunk
     }
 }
 
+enum LineFix
+{
+    Corrupted(Chunk),
+    Incomplete(Vec<Bracket>),
+}
+
 fn find_error(line: &str)
-    -> Chunk
+    -> LineFix
 {
     let mut stack = Vec::new();
     for c in line.chars() {
@@ -69,19 +84,16 @@ fn find_error(line: &str)
         match chunk
         {
         Chunk::Open(b) => {
-            println!("pushing opened chunk {:?}", b);
             stack.push(b)
         },
         Chunk::Close(b) => {
             if let Some(top) = stack.pop() {
-                println!("got chunk {:?} - last opened chunk {:?}", b, top);
                 if top != b {
-                    return Chunk::Close(b);
+                    return LineFix::Corrupted(Chunk::Close(b));
                 }
             }
             else {
-                println!("no more stack, closing unopened chunk");
-                return Chunk::Close(b);
+                return LineFix::Corrupted(Chunk::Close(b));
             }
         },
         Chunk::Invalid => {
@@ -89,22 +101,55 @@ fn find_error(line: &str)
         },
         }
     }
-    Chunk::Invalid
+    LineFix::Incomplete(stack)
 }
 
-fn score_error(error_chunk: &Chunk)
+fn score_corruption(error_chunk: &Chunk)
     -> usize
 {
     match error_chunk
     {
-    // corruption errors
-    &Chunk::Close(Bracket::Round) => 3,
-    &Chunk::Close(Bracket::Square) => 57,
-    &Chunk::Close(Bracket::Curly) => 1197,
-    &Chunk::Close(Bracket::Angular) => 25137,
-
+    Chunk::Close(Bracket::Round) => 3,
+    Chunk::Close(Bracket::Square) => 57,
+    Chunk::Close(Bracket::Curly) => 1197,
+    Chunk::Close(Bracket::Angular) => 25137,
     _ => 0,
     }
+}
+
+fn score_completions(lines: &Vec<LineFix>)
+    -> usize
+{
+    let scores: Vec<usize> = lines.iter().filter( | x | {
+            if let LineFix::Incomplete(_) = x {
+                true
+            } else {
+                false
+            }
+        }).map(|x| {
+            if let LineFix::Incomplete(stack) = x {
+                get_completion_score(stack)
+            } else {
+                panic!();
+            }
+        }).sorted().collect();
+    assert!(scores.len() % 2 == 1);
+    scores[scores.len() / 2]
+}
+
+fn get_completion_score(stack: &Vec<Bracket>)
+    -> usize
+{
+    stack.iter().rev().fold(0, |acc, x| {
+        let x_score = match x
+            {
+            &Bracket::Round => 1,
+            &Bracket::Square => 2,
+            &Bracket::Curly => 3,
+            &Bracket::Angular => 4,
+            };
+        acc*5 + x_score
+    })
 }
 
 #[cfg(test)]
@@ -131,21 +176,38 @@ mod tests
             ];
         for (i,line) in lines.enumerate() {
             let corrupted = find_error(&line);
-            if let Chunk::Invalid = corrupted { continue; }
-            eprintln!("got corrupted {:?}", corrupted);
-            assert_eq!(corrupted, corruptions[i]);
+            if let LineFix::Corrupted(error) = corrupted {
+                eprintln!("got corrupted {:?}", error);
+                assert_eq!(error, corruptions[i]);
+            }
         }
 
     }
+
     #[test]
     /// test finding correct corruptions
     fn test_0x0002()
     {
         let lines = fileops::get_file_lines("test_input.txt");
         let score: usize = lines.map( | x | {
-            let corrupted = find_error(&x);
-            score_error(&corrupted)
+            if let LineFix::Corrupted(corrupted) = find_error(&x){
+                score_corruption(&corrupted)
+            } else {
+                0
+            }
         }).sum();
         assert_eq!(score, 26397);
+    }
+
+    #[test]
+    /// test finding correct completion scores
+    fn test_0x0003()
+    {
+        let lines = fileops::get_file_lines("test_input.txt");
+        let fixes: Vec<LineFix> = lines.map( |x| {
+            find_error(&x)
+        }) .collect();
+        let score = score_completions(&fixes);
+        assert_eq!(score, 288957);
     }
 }
